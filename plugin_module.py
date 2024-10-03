71421e5539934e8e986b85b7ceb9e45f39797b47
Austin Epiphane Yann Tung-Shan Lefebvre <austin.e.lefebvre@gmail.com>
2024-10-02 22:33:21 -0700

    Add message to show it works

--- plugin_module.py
+++ plugin_module.py
@@ -1,3 +1,6 @@
+from napari.utils.notifications import show_info
+
+
 def nellie_plugin_function(napari_viewer):
-    # Access Nellie's outputs and perform actions
-    pass
+    show_info("Hello from my plugin!")
+    print("Hello from my plugin!")
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='my-nellie-plugin',
-    version='0.1.2',
+    version='0.1.3',
     py_modules=['plugin_module'],
 
     install_requires=[
